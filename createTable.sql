6bf20e7d0eb0c4d710131680109ecc86e2630fac
arthur <arthurdesousafernandes@gmail.com>
2023-02-09 17:39:42 -0300

    fix: Create Table

--- createTable.sql
+++ createTable.sql
@@ -3,9 +3,8 @@ CREATE DATABASE movies;
 CREATE TABLE IF NOT EXISTS movies(
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    description TEXT(),
-    duration INTEGER(2,1) NOT NULL,
-    price INTEGER(3,2) NOT NULL
+    description TEXT,
+    duration INTEGER NOT NULL,
+    price INTEGER NOT NULL
 );
-
 
